f4bb58d95a96e450029a5da470aac88f349fe5ce
matheusprata <103780607+matheusprata@users.noreply.github.com>
2023-08-28 15:37:41 -0300

    implementado scanner e solicitado dois parametros

--- src/Contador.java
+++ src/Contador.java
@@ -1,5 +1,15 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Informe um numero inteiro para o primeiro parametro");
+        int parametroUm = scanner.nextInt();
+
+        System.out.println("Informe um numero inteiro para o segundo parametro");
+        int parametroDois = scanner.nextInt();
+
     }
 }
