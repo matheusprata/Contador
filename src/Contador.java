b249cdc33f0913cca856442045729b65f96ba5d0
matheusprata <103780607+matheusprata@users.noreply.github.com>
2023-08-29 10:52:02 -0300

    implementado ParametrosInvalidosExceptional.java, throws e try catch para analisar se o parametro 1 é menor que o parametro dois, informando que o primeiro parametro seja maior

--- src/Contador.java
+++ src/Contador.java
@@ -1,24 +1,30 @@
 import java.util.Locale;
 import java.util.Scanner;
 
-public class Contador {
+public class Contador extends Exception{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Informe um numero inteiro para o primeiro parametro");
         int parametroUm = scanner.nextInt();
-
         System.out.println("Informe um numero inteiro para o segundo parametro");
         int parametroDois = scanner.nextInt();
-        Contar(parametroUm, parametroDois);
+
+        try {
+            contar(parametroUm, parametroDois);
+        } catch (ParametrosInvalidosExceptional exception) {
+            System.out.println("O segundo parâmetro deve ser maior que o primeiro");
+        }
     }
 
-    static void Contar(int parametroUm, int parametroDois){
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosExceptional {
         if(parametroUm < parametroDois){
             int contagem = parametroDois - parametroUm;
             for(int x = 1; x <= contagem; x++){
                 System.out.println("Contador: "+x);
             }
+        }else{
+            throw new ParametrosInvalidosExceptional("O segundo parâmetro deve ser maior que o primeiro");
         }
     }
 }
---
+++ src/ParametrosInvalidosExceptional.java
@@ -0,0 +1,5 @@
+public class ParametrosInvalidosExceptional extends Exception {
+    public ParametrosInvalidosExceptional(String message) {
+        super(message);
+    }
+}
